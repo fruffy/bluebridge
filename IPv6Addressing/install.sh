ada91e3e0f64ba7a1ad73fe155b0864d429e24f1
fruffy <fruffy@cs.ubc.ca>
2017-04-11 09:25:37 -0700

    Further fixes to the horrible install file

--- IPv6Addressing/install.sh
+++ IPv6Addressing/install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -x
 sudo apt-get install ndppd
-./mininet/util/install.sh -a
+cp -f make-ndpproxy ndpproxy/Makefile
+sudo make install -C ndpproxy/ && make all -C ndpproxy/
+cd mininet
+./util/install.sh -a
+cd -
 make -C messaging/
 sudo python run-demo.py
 
