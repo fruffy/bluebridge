5fea11ef5e3c6fe51d1c5581db326c184f1eda64
wantonsolutions <stewbertgrant@gmail.com>
2017-04-14 17:01:36 -0700

    added caching feature

--- TestApps/Cost/single/single.go
+++ TestApps/Cost/single/single.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/gob"
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"runtime/pprof"
 	"sort"
 	"strconv"
@@ -14,29 +16,30 @@ import (
 const DAMP = 0.85
 
 var (
-	itt     = 20
-	tests   = 10
-	profile = false
+	itt         = 20
+	tests       = 1
+	profile     = false
+	updateCache = false
 )
 
 type page struct {
-	incomming_rank float32
+	Incomming_rank float32
 	edges          []int
 }
 
-type page2 struct {
-	incomming_rank float32
-	edge_offset    int
-	num_edges      int
+type Page2 struct {
+	Incomming_rank float32
+	Edge_offset    int
+	Num_edges      int
 }
 
 func newPage(pid int) *page {
-	return &page{incomming_rank: 0.0, edges: make([]int, 0)}
+	return &page{Incomming_rank: 0.0, edges: make([]int, 0)}
 }
 
 func (p *page) String() string {
 	var s string
-	s += fmt.Sprintf("I-Rank: %f ", p.incomming_rank)
+	s += fmt.Sprintf("I-Rank: %f ", p.Incomming_rank)
 	s += "Edges: ["
 	for _, e := range p.edges {
 		s += fmt.Sprintf("%d,", e)
@@ -48,23 +51,23 @@ func (p *page) String() string {
 func printPages() {
 	for i, id := range ids {
 		fmt.Printf("Page: %d,", id)
-		fmt.Printf("Num_Edges: %d,", apages[id].num_edges)
-		fmt.Printf("Edge_Offset: %d,", apages[id].edge_offset)
+		fmt.Printf("Num_Edges: %d,", apages[id].Num_edges)
+		fmt.Printf("Edge_Offset: %d,", apages[id].Edge_offset)
 		fmt.Printf("Rank: %f,", rank[i])
-		fmt.Printf("Incomming-Rank: %f,", apages[id].incomming_rank)
+		fmt.Printf("Incomming-Rank: %f,", apages[id].Incomming_rank)
 		fmt.Printf("EdgeNorm: %f,", edgenorm[i])
 		fmt.Print("Edges: [")
-		for j := 0; j < apages[id].num_edges; j++ {
-			fmt.Printf("%d,", edges[apages[id].edge_offset+j])
+		for j := 0; j < apages[id].Num_edges; j++ {
+			fmt.Printf("%d,", edges[apages[id].Edge_offset+j])
 		}
 		fmt.Println("]")
 	}
 }
 
 /*
-func (p page2) String() string {
+func (p Page2) String() string {
 	var s string
-	s += fmt.Sprintf("I-Rank: %f ", p.incomming_rank)
+	s += fmt.Sprintf("I-Rank: %f ", p.Incomming_rank)
 	s += "Edges: ["
 	for _, e := range p.edges {
 		s += fmt.Sprintf("%d,", e)
@@ -73,16 +76,16 @@ func (p page2) String() string {
 	return s
 }*/
 
-func (p page2) MemoryString() string {
+func (p Page2) MemoryString() string {
 	var s string
 	s += fmt.Sprintf("---------page----------\n")
 	s += fmt.Sprintf("---------rank----------\n")
-	s += fmt.Sprintf("%+v\n", &p.incomming_rank)
+	s += fmt.Sprintf("%+v\n", &p.Incomming_rank)
 	s += fmt.Sprintf("---offset----#edges----\n")
-	s += fmt.Sprintf("%d-%d\n", p.edge_offset, p.num_edges)
+	s += fmt.Sprintf("%d-%d\n", p.Edge_offset, p.Num_edges)
 	s += fmt.Sprintf("---------edges----------\n")
-	for i := 0; i < p.num_edges; i++ {
-		s += fmt.Sprintf("%+v\n", &(edges[p.edge_offset+i]))
+	for i := 0; i < p.Num_edges; i++ {
+		s += fmt.Sprintf("%+v\n", &(edges[p.Edge_offset+i]))
 	}
 	s += "]\n"
 	return s
@@ -93,7 +96,7 @@ var pages map[int]*page
 //apages is a memory map for vertex id's and is potentially sparse
 //index i of apages corresponds to the id of the vertex it represents
 //It is similar to a hash
-var apages []page2
+var apages []Page2
 var ids []int
 var edgenorm []float32
 var rank []float32
@@ -136,24 +139,24 @@ func main() {
 func pageRank3(rounds int) {
 	var outrank float32
 	var alpha = (1 - DAMP) / float32(len(pages))
-	var count int64
+	//var count int64
 	for i := 0; i < rounds; i++ {
 		//printPages()
 		for j := 0; j < len(ids); j++ {
 			outrank = rank[j] / edgenorm[j]
 			//fmt.Printf("page %d\n", ids[j])
-			for k := 0; k < apages[ids[j]].num_edges; k++ {
+			for k := 0; k < apages[ids[j]].Num_edges; k++ {
 				//send message to another page
-				apages[edges[apages[ids[j]].edge_offset+k]].incomming_rank += outrank
-				//fmt.Printf("page: %d edge %d\n", ids[j], edges[apages[ids[j]].edge_offset+k])
-				count++
+				apages[edges[apages[ids[j]].Edge_offset+k]].Incomming_rank += outrank
+				//fmt.Printf("page: %d edge %d\n", ids[j], edges[apages[ids[j]].Edge_offset+k])
+				//count++
 			}
 		}
-		fmt.Println(count)
-		count = 0
+		//fmt.Println(count)
+		//count = 0
 		for j := 0; j < len(ids); j++ {
-			rank[j] = alpha + (DAMP * apages[ids[j]].incomming_rank)
-			//apages[j].incomming_rank[j] = 0.0
+			rank[j] = alpha + (DAMP * apages[ids[j]].Incomming_rank)
+			//apages[j].Incomming_rank[j] = 0.0
 		}
 		//printPages()
 	}
@@ -168,11 +171,11 @@ func pageRank2(rounds int) {
 			outrank = rank[j] / edgenorm[j]
 
 			for k := 0; k < len(apages[j].edges); k++ {
-				apages[apages[j].edges[k]].incomming_rank += outrank
+				apages[apages[j].edges[k]].Incomming_rank += outrank
 			}
 		}
 		for j := 0; j < len(ids); j++ {
-			rank[j] = alpha + (DAMP * apages[j].incomming_rank)
+			rank[j] = alpha + (DAMP * apages[j].Incomming_rank)
 		}
 	}
 }*/
@@ -190,20 +193,26 @@ func pageRank(rounds int) {
 
 func sendMessages(id int) {
 	var outrank float32
-	var p page2
+	var p Page2
 	p = apages[id]
-	outrank = rank[id] / float32((p.num_edges + 1))
-	for i := 0; i < p.num_edges; i++ {
-		apages[p.edges[i]].incomming_rank += outrank
+	outrank = rank[id] / float32((p.Num_edges + 1))
+	for i := 0; i < p.Num_edges; i++ {
+		apages[p.edges[i]].Incomming_rank += outrank
 	}
 }
 
 func update(id int) {
-	apages[id].rank = (1-DAMP)*(1.0/float32(len(pages))) + (DAMP * apages[id].incomming_rank)
+	apages[id].rank = (1-DAMP)*(1.0/float32(len(pages))) + (DAMP * apages[id].Incomming_rank)
 }
 */
 
 func parseFile(filename string) {
+	base := path.Base(filename)
+	if !updateCache && fileCached(base) {
+		readCache(base)
+		return
+	}
+	fmt.Println("reading from file")
 	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("%s", err.Error)
@@ -218,7 +227,6 @@ func parseFile(filename string) {
 	for scanner.Scan() {
 		buf = scanner.Text()
 		//fmt.Println(buf)
-
 		for i = 0; i < len(buf); i++ {
 			if buf[i] == '\t' {
 				a, err := strconv.Atoi(buf[0:i])
@@ -243,14 +251,14 @@ func parseFile(filename string) {
 					max = b
 				}
 				pages[a].edges = append(pages[a].edges, b)
-				pages[b].incomming_rank = 0.0
+				pages[b].Incomming_rank = 0.0
 				e++
 			}
 		}
 	}
 	ids = make([]int, len(pages))
 	//apages and edges are the main structures
-	apages = make([]page2, max+1)
+	apages = make([]Page2, max+1)
 	edges = make([]int, e)
 	//edgenorm and rank are optimizations
 	edgenorm = make([]float32, len(pages))
@@ -263,22 +271,86 @@ func parseFile(filename string) {
 	}
 	sort.Ints(ids)
 	for i, id := range ids {
-		apages[id].num_edges = len(pages[id].edges)
+		apages[id].Num_edges = len(pages[id].edges)
 		//fmt.Printf("#edges %d %d", id, len(pages[id].edges))
 		rank[i] = 1.0
-		edgenorm[i] = float32(apages[id].num_edges + 1)
-		apages[id].edge_offset = k
+		edgenorm[i] = float32(apages[id].Num_edges + 1)
+		apages[id].Edge_offset = k
 		for l := range pages[id].edges {
 			edges[k] = pages[id].edges[l]
 			k++
 		}
 	}
 
+	WriteToFiles(base)
+
 	//for i := range apages {
 	//	fmt.Println(apages[i].MemoryString())
 	//}
 
 	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func WriteToFiles(filename string) {
+	//make a cache
+	_, err := os.Stat("./cache/")
+	if os.IsNotExist(err) {
+		os.Mkdir("./cache/", 0711)
+	}
+	//make a dir
+	_, err = os.Stat("./cache/" + filename)
+	if os.IsNotExist(err) {
+		os.Mkdir("./cache/"+filename, 0711)
+	}
+	dir := "./cache/" + filename + "/"
+	writeFile(dir+"apages", apages)
+	writeFile(dir+"edgenorm", edgenorm)
+	writeFile(dir+"ids", ids)
+	writeFile(dir+"rank", rank)
+	writeFile(dir+"edges", edges)
+}
+
+func writeFile(filename string, item interface{}) {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	enc := gob.NewEncoder(f)
+	err = enc.Encode(item)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fileCached(filename string) bool {
+	_, err := os.Stat("./cache/" + filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func readCache(filename string) {
+	fmt.Println("reading from cache")
+	dir := "./cache/" + filename + "/"
+	readCachefile(dir+"apages", &apages)
+	readCachefile(dir+"edgenorm", &edgenorm)
+	readCachefile(dir+"ids", &ids)
+	readCachefile(dir+"rank", &rank)
+	readCachefile(dir+"edges", &edges)
+}
+
+func readCachefile(objectname string, object interface{}) {
+	f, err := os.Open(objectname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dec := gob.NewDecoder(f)
+	err = dec.Decode(object)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
