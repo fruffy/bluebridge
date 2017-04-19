76534035c2b17fc629ad43bd2e0118c770ded8ba
fruffy <fruffy@cs.ubc.ca>
2017-04-18 17:16:31 -0700

    install script

--- IPv6Addressing/install.sh
+++ IPv6Addressing/install.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 set -x
+
 sudo apt-get install ndppd
-cp -f make-ndpproxy ndpproxy/Makefile
-sudo make install -C ndpproxy/ && make all -C ndpproxy/
-cd mininet
-./util/install.sh -a
-cd -
+
+if [[ $? > 0 ]]
+then
+	sudo apt-get install libnl-3-dev
+	sudo apt-get install libnl-route-3-dev
+	cp -f make-ndpproxy ndpproxy/Makefile
+	sudo make -C ndpproxy/ install && make -C ndpproxy/ all
+fi
+
+sudo apt-get install mininet
+if [[ $? > 0 ]]
+then
+	./mininet/util/install.sh -a
+fi
+
 make -C messaging/
 sudo python run-demo.py
 
