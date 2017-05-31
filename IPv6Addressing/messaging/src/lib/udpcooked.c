48a10801844f76e031ab10c4ff47d07392ea77ea
fruffy <fruffy@cs.ubc.ca>
2017-05-31 11:56:05 -0700

    improve checksum function slightly...

--- IPv6Addressing/messaging/src/lib/udpcooked.c
+++ IPv6Addressing/messaging/src/lib/udpcooked.c
@@ -263,7 +263,61 @@ uint16_t checksum (uint16_t *addr, int len) {
 
     return (answer);
 }
+//TODO: Adapt to IPv6
+unsigned short checksum2(const char *buf, unsigned size)
+{
+    unsigned long long sum = 0;
+    const unsigned long long *b = (unsigned long long *) buf;
 
+    unsigned t1, t2;
+    unsigned short t3, t4;
+
+    /* Main loop - 8 bytes at a time */
+    while (size >= sizeof(unsigned long long))
+    {
+        unsigned long long s = *b++;
+        sum += s;
+        if (sum < s) sum++;
+        size -= 8;
+    }
+
+    /* Handle tail less than 8-bytes long */
+    buf = (const char *) b;
+    if (size & 4)
+    {
+        unsigned s = *(unsigned *)buf;
+        sum += s;
+        if (sum < s) sum++;
+        buf += 4;
+    }
+
+    if (size & 2)
+    {
+        unsigned short s = *(unsigned short *) buf;
+        sum += s;
+        if (sum < s) sum++;
+        buf += 2;
+    }
+
+    if (size)
+    {
+        unsigned char s = *(unsigned char *) buf;
+        sum += s;
+        if (sum < s) sum++;
+    }
+
+    /* Fold down to 16 bits */
+    t1 = sum;
+    t2 = sum >> 32;
+    t1 += t2;
+    if (t1 < t2) t1++;
+    t3 = t1;
+    t4 = t1 >> 16;
+    t3 += t4;
+    if (t3 < t4) t3++;
+
+    return ~t3;
+}
 // Build IPv6 UDP pseudo-header and call checksum function (Section 8.1 of RFC 2460).
 uint16_t udp6_checksum (struct ip6_hdr iphdr, struct udphdr udphdr, uint8_t *payload, int payloadlen) {
     char buf[IP_MAXPACKET];
@@ -332,6 +386,7 @@ uint16_t udp6_checksum (struct ip6_hdr iphdr, struct udphdr udphdr, uint8_t *pay
         chksumlen++;
     }
 
+ //   return checksum2 ((const char *) buf, chksumlen);
     return checksum ((uint16_t *) buf, chksumlen);
 }
 
