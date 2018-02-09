7214e6eb55691ece0eac59f8819f0c9ae3b1bf10
Unknown <fabian.ruffy@gmail.com>
2018-02-08 19:56:38 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -x
+# set -x
 
 cd includes
-read -r -p "Do you want to install the DPDK version? [y/N]" response
+read -r -p "Do you want to install the DPDK version? [y/N]: " response
 response=${response,,} # tolower
-if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
-    sudo ./setup_includes.sh
+if [[ $response =~ ^(yes|y) ]]; then
+    sudo ./setup_includes.sh -d
     make dpdk
 else 
-    sudo ./setup_includes.sh -d
+    sudo ./setup_includes.sh
     make
 fi
 cd ..
