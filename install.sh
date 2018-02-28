d38c8e987694a180e147a0577970d4c67f28d069
Unknown <fabian.ruffy@gmail.com>
2018-02-27 19:04:08 -0800

    Final fix of the build setup (I hope).

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # set -x
 
-cd includes
 read -r -p "Do you want to install the DPDK version? [y/N]: " response
 response=${response,,} # tolower
 if [[ $response =~ ^(yes|y) ]]; then
-    ./setup_includes.sh -d
-    cd ..
+    (cd includes && ./setup_includes.sh -d)
     make dpdk
 else 
-    ./setup_includes.sh
-    cd ..
+    (cd includes && ./setup_includes.sh)
     make
 fi
 sudo chown -R $USER:$USER .
