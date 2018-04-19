4f21b0827231e53cd7b704eb8b5917d3968dfb03
Amanda Carbonari <acarb95@gmail.com>
2018-04-19 16:13:36 -0700

    Updated netperf test

--- ip6/netperf_test.sh
+++ ip6/netperf_test.sh
@@ -1,19 +1,14 @@
 #! /bin/bash
 
 COUNTER=0
-OPERATION="UDP"
-BUF_SIZE=2097153
 
 while [ $COUNTER -lt 22 ]; do
 	SIZE=$((2**$COUNTER))
-	echo "Size: $SIZE, buf size: $BUF_SIZE"
+	echo "Size: $SIZE"
 
-	if [ $OPERATION = "TCP" ]; then
-		echo "netperf -t TCP_RR -H 0:0:102:: -v 2 -- -r $SIZE,$SIZE -s $BUF_SIZE,$BUF_SIZE -S $BUF_SIZE,$BUF_SIZE" >> netperf_tcp_rr.txt
-		netperf -t TCP_RR -H 0:0:102:: -v 2 -- -r $SIZE,$SIZE -s $BUF_SIZE,$BUF_SIZE -S $BUF_SIZE,$BUF_SIZE >> netperf_tcp_rr.txt
-	else
-		echo "netperf -t UDP_RR -H 0:0:102:: -v 2 -- -r $SIZE,$SIZE -s $BUF_SIZE,$BUF_SIZE -S $BUF_SIZE,$BUF_SIZE" >> netperf_udp_rr.txt
-		netperf -t UDP_RR -H 0:0:102:: -v 2 -- -r $SIZE,$SIZE -s $BUF_SIZE,$BUF_SIZE -S $BUF_SIZE,$BUF_SIZE >> netperf_udp_rr.txt
-	fi
+	echo "netperf -t TCP_RR -H 0:0:102:: -v 2 -- -r $SIZE,$SIZE -o P50_LATENCY,P90_LATENCY,P99_LATENCY,MEAN_LATENCY,STDDEV_LATENCY" >> netperf_tcp_rr.txt
+	netperf -t TCP_RR -H 0:0:102:: -v 2 -- -r $SIZE,$SIZE -o P50_LATENCY,P90_LATENCY,P99_LATENCY,MEAN_LATENCY,STDDEV_LATENCY >> netperf_tcp_rr.txt
+	echo "netperf -t UDP_RR -H 0:0:102:: -v 2 -- -r $SIZE,$SIZE -o P50_LATENCY,P90_LATENCY,P99_LATENCY,MEAN_LATENCY,STDDEV_LATENCY" >> netperf_udp_rr.txt
+	netperf -t UDP_RR -H 0:0:102:: -v 2 -- -r $SIZE,$SIZE -o P50_LATENCY,P90_LATENCY,P99_LATENCY,MEAN_LATENCY,STDDEV_LATENCY >> netperf_udp_rr.txt
 	let COUNTER=COUNTER+1
 done
